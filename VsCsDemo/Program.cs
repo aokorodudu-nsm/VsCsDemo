a534be8bf588f0d20f2844a388b33c1769d1d285
Tony Okorodudu <tony.okorodudu@NationstarMail.com>
2016-06-23 18:11:19 -0500

    Static imports

--- VsCsDemo/Program.cs
+++ VsCsDemo/Program.cs
@@ -10,7 +10,12 @@ namespace VsCsDemo
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Yellow;
 
+            Console.WriteLine($"Awesome color selection!!!  Thank goodness today is {DayOfWeek.Friday}.");
+
+            Console.ReadKey(true);
         }
     }
 }
