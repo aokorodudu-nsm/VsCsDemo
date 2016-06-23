13920ceadc4160d71e8dc5c2a109feefb9e25218
Tony Okorodudu <tony.okorodudu@NationstarMail.com>
2016-06-23 18:18:12 -0500

    String interpolation

--- VsCsDemo/Program.cs
+++ VsCsDemo/Program.cs
@@ -10,6 +10,18 @@ namespace VsCsDemo
     {
         static void Main(string[] args)
         {
+            var firstName = "Tony";
+            var lastName = "Okorodudu";
+            var action = "standing";
+            var day = DateTime.Today;
+            var location = "Conference Room";
+
+            // Tony Okorodudu is standing in the Conference Room on 
+            var message = string.Format("{0} {1} is {2} in the {3} on {4:MMM d, yyyy}", firstName, lastName, action, location, day);
+
+            Console.WriteLine(message);
+
+            Console.ReadKey(true);
         }
     }
 }
