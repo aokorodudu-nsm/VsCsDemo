348632cfb7bce2d2afbee88cacefeccbfbca0404
Tony Okorodudu <tony.okorodudu@NationstarMail.com>
2016-06-23 18:35:15 -0500

    Null conditional operator

--- VsCsDemo/Program.cs
+++ VsCsDemo/Program.cs
@@ -10,6 +10,33 @@ namespace VsCsDemo
     {
         static void Main(string[] args)
         {
+            var borrower = new Borrower { FirstName = "Tony" };
+
+            if (borrower != null && borrower.SubjectProperty != null && borrower.SubjectProperty.AddressLine1 != null)
+            {
+                Console.WriteLine(borrower.SubjectProperty.AddressLine1);
+            }
+            else
+            {
+                Console.WriteLine("TBD");
+            }
+
+            Console.ReadKey(true);
         }
+    }
+
+    class Borrower
+    {
+        public string FirstName { get; set; }
+        public Address SubjectProperty { get; set; }
+    }
+
+    class Address
+    {
+        public string AddressLine1 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
     }
 }
