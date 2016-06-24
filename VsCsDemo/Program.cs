78230e3f172c1015481f54e01d77773cc178e7df
Tony Okorodudu <tony.okorodudu@NationstarMail.com>
2016-06-23 20:26:24 -0500

    index initializer before

--- VsCsDemo/Program.cs
+++ VsCsDemo/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json.Linq;
+using System;
 
 namespace VsCsDemo
 {
@@ -10,6 +7,13 @@ namespace VsCsDemo
     {
         static void Main(string[] args)
         {
+            var person = new JObject();
+            person["FirstName"] = "Tony";
+            person["LastName"] = "Okorodudu";
+
+            Console.WriteLine(person);
+
+            Console.ReadKey(true);
         }
     }
 }
