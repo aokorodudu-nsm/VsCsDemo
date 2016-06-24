5e2b62737e5d656d43cf7ff606cefbe7e7ee04da
Tony Okorodudu <tony.okorodudu@NationstarMail.com>
2016-06-23 19:44:56 -0500

    expression bodied members before

--- VsCsDemo/Program.cs
+++ VsCsDemo/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VsCsDemo
 {
@@ -10,6 +6,21 @@ namespace VsCsDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"The class name is: {ClassName}");
+
+            Console.WriteLine($"The sum of 1 and 2 is {Add(1, 2)}");
+
+            Console.ReadKey(true);
+        }
+
+        static string ClassName
+        {
+            get { return typeof(Program).FullName; }
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
