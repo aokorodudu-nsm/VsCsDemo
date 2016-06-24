4ed5ca5d9b8eb8a7aceb07140b3eeacd47da16a9
Tony Okorodudu <tony.okorodudu@NationstarMail.com>
2016-06-23 19:31:21 -0500

    auto property initializers

--- VsCsDemo/Program.cs
+++ VsCsDemo/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VsCsDemo
 {
@@ -10,6 +6,25 @@ namespace VsCsDemo
     {
         static void Main(string[] args)
         {
+            var person = new Person(1);
+            Console.WriteLine($"The id for {person.FirstName} is {person.Id}");
+
+            Console.ReadKey(true);
         }
+    }
+
+    class Person
+    {
+        private readonly int _id;
+
+        public Person(int id)
+        {
+            _id = id;
+            FirstName = "Tony";
+        }
+
+        public int Id { get { return _id; } }
+
+        public string FirstName { get; set; }
     }
 }
