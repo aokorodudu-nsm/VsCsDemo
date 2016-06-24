c566d4265bec3cb95a8a9132573dbf8a64a52c88
Tony Okorodudu <tony.okorodudu@NationstarMail.com>
2016-06-23 20:53:03 -0500

    await in finally before

--- VsCsDemo/Program.cs
+++ VsCsDemo/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace VsCsDemo
@@ -10,6 +8,39 @@ namespace VsCsDemo
     {
         static void Main(string[] args)
         {
+            var movieSummary = FindMovieAsync("Independence Day").Result;
+
+            Console.WriteLine(movieSummary);
+
+            Console.ReadKey(true);
+        }
+
+        public static async Task<string> FindMovieAsync(string title)
+        {
+            using (var client = new HttpClient { BaseAddress = new Uri("http://www.omdbapi.com/") })
+            {
+                try
+                {
+                    return await client.GetStringAsync($"?t={title}&r=json");
+                }
+                finally
+                {
+                    // Not possible prior to C# 6
+                    // await Logger.WriteAsync("All done");
+                }
+            }
+        }
+    }
+
+
+    class Logger
+    {
+        public static async Task WriteAsync(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+            await Task.FromResult(0);
         }
     }
 }
