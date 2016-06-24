09b765d30b1426c3d70313e28d9b0dc1252f007c
Tony Okorodudu <tony.okorodudu@NationstarMail.com>
2016-06-23 19:17:27 -0500

    Static import for extension methods before

--- VsCsDemo/Program.cs
+++ VsCsDemo/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VsCsDemo
 {
@@ -10,6 +7,13 @@ namespace VsCsDemo
     {
         static void Main(string[] args)
         {
+            var numbers = Enumerable.Range(0, 100);
+
+            var divisibleBy5 = numbers.Where(number => number % 5 == 0);
+
+            Console.WriteLine(string.Join(Environment.NewLine, divisibleBy5));
+
+            Console.ReadKey(true);
         }
     }
 }
